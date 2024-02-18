a91f25ca7d5d2b443584b5f03758b8f6d9fa8abd
Mir-Aadil1 <ermiraadil@gmail.com>
2024-02-18 12:51:59 +0530

    added dll.py

---
+++ LinkedLists/dll.py
@@ -0,0 +1,35 @@
+class DLLNODE:
+
+    def __init__(self, data):
+        self.data = data 
+        self.next = None
+        self.previous = None
+
+
+
+    def get_data(self):
+        """Return the self.data attribute"""
+        return self.data 
+    
+    def __repr__(self):
+        return "SLLNode Object: data={}".format(self.data)
+
+    def set_data(self, new_data):
+        """Replace the existing value of the self.data attribute with new_data parameters"""
+        self.data = new_data
+
+    def get_next(self):
+        """Return the self.next attribute"""
+        return self.next
+    
+    def get_previous(self):
+        """Return the self.previous attribute"""
+        return self.previous
+
+    def set_previous(self, new_previous):
+        """Replace the existing value of the self.previous attribute with new_previous parameter"""
+        self.previous = new_previous
+
+    def set_next(self, new_next):
+        """Replace the existing value of the self.next attribute with new_next parameters"""
+        self.next = new_next
