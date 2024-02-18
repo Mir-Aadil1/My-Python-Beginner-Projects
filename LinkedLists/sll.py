5bfba16d64a1eb692087c4786a44d3258e935f64
Mir-Aadil1 <ermiraadil@gmail.com>
2024-02-18 13:03:13 +0530

    Added SLL class to sll.py

--- LinkedLists/sll.py
+++ LinkedLists/sll.py
@@ -1,4 +1,4 @@
-class SLLNODE:
+class SLLNode:
 
     def __init__(self, data):
         self.data = data 
@@ -24,3 +24,36 @@ class SLLNODE:
     def set_next(self, new_next):
         """Replace the existing value of the self.next attribute with new_next parameters"""
         self.next = new_next
+
+
+#  Linked lists don't contain nodes, instead they have a head attribute that points to first node of the linked list if one exists , otherwise to None 
+        
+
+
+
+class SLL:
+    def __init__(self):
+        self.head = None 
+
+    def __repr__(self):
+        return "SLL object: head={}".format(self.head)
+    
+
+    def is_empty(self):
+        """Returns True if the Linked List is empty otherwise , returns False"""
+        return self.head is None  # self.head == None 
+
+    def add_front(self, new_data):
+        pass 
+
+    def size(self):
+        pass 
+
+    def search(self, data):
+        pass 
+
+    def remove(self, data):
+        pass 
+
+
+
